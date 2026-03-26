db99f613d8a44613a0ea586cdf86ba0d7d41a238
Anmol7860-bit <anmolbhandare7860@gmail.com>
2026-03-26 15:42:50 +0530

    Created tables and  imported csv files

---
+++ data analytics.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE DATA_ANALYTICS;
+
+
+CREATE TABLE GOLD_FACT_SALES(
+  order_number varchar(150),
+  product_key INTEGER,
+  customer_key INTEGER,
+  order_date TIMESTAMP,
+  shipping_date TIMESTAMP,
+  due_date TIMESTAMP,
+  sales_amount INTEGER,
+  quantity INTEGER,
+  price INTEGER
+);
+
+CREATE TABLE GOLD_DIM_PRODUCTS(
+  product_key serial,
+  product_id INTEGER,
+  product_number varchar(150),
+  product_name varchar(150),
+  category_id varchar(150),
+  category varchar(150),
+  subcategory varchar(150),
+  maintenance varchar(150),
+  cost INTEGER,
+  product_line varchar(150),
+  start_date timestamp
+);
+
+CREATE TABLE GOLD_DIM_CUSTOMERS(
+  customer_key serial,
+  customer_id INTEGER,
+  customer_number varchar(150),
+  first_name varchar(150),
+  last_name varchar(150),
+  country varchar(150),
+  martial_status varchar(150),
+  gender varchar(150),
+  birthdate TIMESTAMP,
+  create_date TIMESTAMP
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
