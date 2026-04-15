04ea3aaf4066e9bd8407c3044b5269bd3aafb137
Anmol7860-bit <anmolbhandare7860@gmail.com>
2026-04-15 20:52:49 +0530

    Update data analytics.sql

--- data analytics.sql
+++ data analytics.sql
@@ -320,7 +320,7 @@ Purpose:
 Highlights:
 1.⁠ ⁠Gathers essential fields such as Product name,category,subcategory and cost.
 2.⁠ ⁠Segments products by revenue to identify High-performance,Mid-Range or Low-Performance.
-3.⁠ ⁠Aggregates customer-level metrics:
+3.⁠ ⁠Aggregates product-level metrics:
    - total orders
    - total sales
    - total quantity sold
@@ -335,7 +335,27 @@ Highlights:
 ============================================================
 */
 
-	
+WITH base_query AS(
+/*
+============================================================
+1) Base Query: Retrives core columns from fact_sales and dim_products
+============================================================ */
+SELECT 
+	f.order_number,
+	f.order_date,
+	f.customer_key,
+	f.sales_amount,
+	f.quantity,
+	p.product_key,
+	p.product_name,
+	p.category,
+	p.subcategory,
+	p.cost
+FROM gold.fact_sales f LEFT JOIN 
+gold.dim_products p 
+ON f.product_key=p.product_key
+WHERE order_date IS NOT NULL -- only consider valid sales dates 
+),	
 
 
 
