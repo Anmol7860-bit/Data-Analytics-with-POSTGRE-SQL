4f182baf6477ac1bf15304fd232aec22be1a18fb
Anmol7860-bit <anmolbhandare7860@gmail.com>
2026-04-16 06:30:14 +0530

    Update data analytics.sql

--- data analytics.sql
+++ data analytics.sql
@@ -351,11 +351,11 @@ SELECT
 	p.category,
 	p.subcategory,
 	p.cost
-FROM gold.fact_sales f LEFT JOIN 
-gold.dim_products p 
+FROM gold_fact_sales f LEFT JOIN 
+gold_dim_products p 
 ON f.product_key=p.product_key
 WHERE order_date IS NOT NULL -- only consider valid sales dates 
-),	
+),
 
 product_aggregations AS(
 /*============================================================
@@ -368,14 +368,14 @@ SELECT
 	category,
 	subcategory,
 	cost,
-	DATE_PART('month',MIN(order_date),MAX(order_date)) AS lifespan,
+	DATE_PART('month',AGE(MAX(order_date),MIN(order_date))) AS lifespan,
 	MAX(order_date) AS last_sale_date,
 	COUNT(DISTINCT order_number) AS total_orders,
 	COUNT(DISTINCT customer_key) AS total_customers,
 	SUM(sales_amount) AS total_sales,
 	SUM(quantity) AS total_quantity,
-	ROUND(AVG(CAST(sales_amount as FLOAT)/NULLIF(quantity,0)),1) AS avg_selling_price
-FROM base query
+	ROUND(AVG(CAST(sales_amount as FLOAT)/NULLIF(quantity,0))::NUMERIC,1) AS avg_selling_price
+FROM base_query
 
 GROUP BY 
 	product_key,
@@ -384,8 +384,51 @@ GROUP BY
 	subcategory,
 	cost
 )
+/*============================================================
+3)Final Query:Combines all product results into one output
+============================================================*/
+SELECT 
+	product_key,
+	product_name,
+	category,
+	subcategory,
+	cost,
+	last_sale_date,
+	DATE_PART('month',AGE(last_sale_date)) AS recency_in_months,
+	CASE
+		WHEN total_sales >50000 THEN 'High-performer'
+		WHEN total_sales >=10000 THEN 'Mid-Range'
+		ELSE 'Low-Performer'
+	END AS product_segment,
+	lifespan,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_customers,
+	avg_selling_price,
+	-- Average Order Revenue (AOR)
+	CASE
+		WHEN total_orders =0 THEN 0 
+		ELSE total_sales/total_orders
+	END AS avg_order_revenue,
+
+	--Average Monthly Revenue
+	CASE
+		WHEN lifespan=0 THEN total_sales
+		ELSE total_sales/lifespan
+	END AS avg_monthly_revenue
+FROM product_aggregations;
+	
+	
 
 
+
+
+
+
+
+
+	
 
 
 
